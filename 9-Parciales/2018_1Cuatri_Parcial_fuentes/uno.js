7baf5999ef037a5c318d69ad585211c9de74ebfc
alumno <alumno>
2019-02-11 22:16:49 -0300

    Parcial Ejercicio 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,9 @@
 
 function mostrar()
 {
-
+ ancho = parseInt(prompt("Ingrese un ancho"));
+ 
+ largo = parseInt(prompt("ingrese un largo"));
+ perimetro= (ancho*2)+(largo*2)
+ alert("El perimetro es "+ perimetro)
 }
