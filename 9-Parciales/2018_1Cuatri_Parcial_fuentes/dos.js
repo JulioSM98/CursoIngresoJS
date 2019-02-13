093a9b5bba72bc7a603d4ee486bbbf2ca5f4c140
alumno <alumno>
2019-02-13 19:27:05 -0300

    Ejercicio 2 Correccion

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,6 +1,10 @@
 function mostrar()
 {
-    name = document.getElementById('elNombre').value;
-    local = document.getElementById('laLocalidad').value;
-    alert("usted es "+name+" y vive en la localidad de "+local)
+    var Efectivo;
+    var Recargo;
+    var PrecioFinal;
+    Efectivo=prompt("Ingrese el precio del producto");
+    Recargo= parseInt(Efectivo)*0.10;
+    PrecioFinal= parseInt(Efectivo)+Recargo;
+    alert("El precio del producto en efectivo es :$"+Efectivo+", con tarjeta tiene un recargo del 10% que seria $"+Recargo+" de recargo, costandole final $"+PrecioFinal)
 }
