327270762f45c9e591dceae5cdd332a0590a61aa
alumno <alumno>
2019-02-13 19:13:59 -0300

    Ejercicio 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,6 @@
 function mostrar()
 {
-  
+    name = document.getElementById('elNombre').value;
+    local = document.getElementById('laLocalidad').value;
+    alert("usted es "+name+" y vive en la localidad de "+local)
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,9 +1,10 @@
 
 function mostrar()
 {
- ancho = parseInt(prompt("Ingrese un ancho"));
- 
- largo = parseInt(prompt("ingrese un largo"));
- perimetro= (ancho*2)+(largo*2)
- alert("El perimetro es "+ perimetro)
+    name1=prompt("Ingrese un nombre");
+    name2=prompt("ingrese otro nombre");
+    age1=parseInt(prompt("Ingrese la edad del primer nombre"));
+    age2=parseInt(prompt("Ingrese la edad del segundo nombre"));
+    sumAge=age1+age2;
+   alert("ustedes son "+name1+" y "+name2+", sus edades son "+age1+" y "+age2+" ,y la suma de edades es "+sumAge)
 }
