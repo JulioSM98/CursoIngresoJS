a6928b17005a035c8780111d46bc1524896c14c9
alumno <alumno>
2019-02-13 19:19:19 -0300

    Correccion Ejercicio 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,10 +1,15 @@
 
 function mostrar()
 {
+    var name1;
+    var name2;
+    var age1;
+    var age2;
+    var sumAge;
     name1=prompt("Ingrese un nombre");
     name2=prompt("ingrese otro nombre");
-    age1=parseInt(prompt("Ingrese la edad del primer nombre"));
-    age2=parseInt(prompt("Ingrese la edad del segundo nombre"));
-    sumAge=age1+age2;
+    age1=prompt("Ingrese la edad del primer nombre");
+    age2=prompt("Ingrese la edad del segundo nombre");
+    sumAge=parseInt(age1)+parseInt(age2);
    alert("ustedes son "+name1+" y "+name2+", sus edades son "+age1+" y "+age2+" ,y la suma de edades es "+sumAge)
 }
