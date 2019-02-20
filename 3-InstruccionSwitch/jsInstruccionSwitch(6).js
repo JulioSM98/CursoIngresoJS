5a4ff4e19a0dde44e51cb85a2510e3002b7cad77
alumno <alumno>
2019-02-20 19:00:48 -0300

    Clase 20/02

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -3,23 +3,27 @@ function mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 var message="";
-var cases;
-
-if(laHora>"6" && laHora<"12"){
-    cases=1;
-}else{
-    if(laHora>"11" && laHora<"20"){
-        cases=2;
-    }else{
-        if(laHora>"19" && laHora<"24" || laHora>="0" && laHora<"7"){
-            cases=3;
-        }else{
-            cases="";
+laHora=parseInt(laHora);
+if(laHora>6 && laHora<12)
+{
+    laHora=1;
+}else
+    {
+    if(laHora>11 && laHora<=19)
+    {
+        laHora=2;
+    }else
+        {
+        if(laHora>=20 && laHora<=24 || laHora>=0 && laHora<7)
+        {
+            laHora=3;
+        }else
+        {
+            laHora="";
         }
     }
-
 }
-switch(cases){
+switch(laHora){
     case 1:
     message="Es de mañana.";
     break;
