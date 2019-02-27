0a7f18d1cec9b8b79a0687589f243daa205c2493
JulioSM <juliosejas98@gmail.com>
2019-02-27 16:51:57 -0300

    Parciales finalizados

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,59 @@
 function mostrar()
 {
+    var entrada;
+    var decision=true;
+    var nombre;
+    var peso;
+    var temperatura;
+    var temperaturaPares=0;
+    var pesoMax=0;
+    var animalesFrios=0;
+    var cont=0;
+    var pesoMax0g=0;
+    var pesoMin0g=1000;
+    while(decision==true){
+        entrada=prompt("ingrese el nombre, peso y temperatura del habita","marco, 40, -30");
+        nombre=entrada.split(", ")[0];
+        peso=entrada.split(", ")[1];
+        peso=parseInt(peso);
+        temperatura=entrada.split(", ")[2];
+        temperatura=parseInt(temperatura);
+        while(isNaN(peso)==true||peso<1||peso>100){
+            peso=prompt("reingrese el peso");
+            peso=parseInt(peso);
+        }
+        while(isNaN(temperatura)==true||temperatura<-30||temperatura>30){
+            temperatura=prompt("Error en la temperatura, reingrese la temperatura del habita");
+            temperatura=parseInt(temperatura);
+        }
+        //A
+        if(temperatura%=0){
+            temperaturaPares=temperaturaPares+1;
+        }
+        //B
+        if(peso>pesoMax){
+            pesoMax=peso;
+            nombreMax=nombre;
+            temperaturaMax=temperatura;            
+        }
+        //CyF
+        if(temperatura<0){
+            animalesFrios=animalesFrios+1;
+            if(peso>pesoMax0g){
+                pesoMax0g=peso;
+            }else{
+                if(peso<pesoMin0g){
+                    pesoMin0g=peso
+                }
+            }
 
+        }
+        //d
+        cont=cont+1
+        promedio=peso+peso;
+
+        decision=confirm("continuar?")
+    }
+    promedio=promedio/cont;
+    document.write("a")
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,14 +1,73 @@
+// Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre -100 y 100 (validar) hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+// a) La cantidad de números pares.
+// b) La cantidad de números impares.
+// c) La cantidad de ceros.
+// d) El promedio de todos los números positivos ingresados.
+// e) La suma de todos los números negativos.
+// f) El número y la letra del máximo y el mínimo.
 function mostrar()
 {
+    var entrada;
     var numero;
     var letra;
     var decision=true;
+    var par=0;
+    var impar=0;
+    var ceros=0;
+    var promedio;
+    var positivo=0;
+    var contador=0;
+    var negativos=0;
+    var maximo=0;
+    var minimo=100;
+    var letraMIN;
+    var letraMAX;
     while(decision==true){
-        numero=prompt("ingrese un numero entre -100 y 100");
+        entrada=prompt("Ingrese una letra y numero, sin espacios");
+
+        letra=entrada.split(",")[0];
+        numero=entrada.split(",")[1]
         numero=parseInt(numero);
-        while(isNaN(numero)==true || numero<-100 && numero>100){
+        while( isNaN(numero)==true || numero<-100 || numero>100){
             numero=prompt("Reingrese un numero entre -100 y 100");
             numero=parseInt(numero);
         }
+        //Ceros,pares e impares
+        if(numero==0){
+            ceros=ceros+1;
+        }else{
+            if(numero%2==0){
+                par=par+1;
+            }else{
+                impar=impar+1;
+            }
+        }
+        //promedios
+        if(numero>0){
+            contador+=1;
+            positivo=positivo+numero
+        }else{
+            negativos=negativos+numero;
+        }
+        //Maximos y minimos
+        if(numero>maximo){
+            maximo=numero;
+            letraMAX=letra;
+        }else{
+                if(numero<minimo){
+                    minimo=numero;
+                    letraMIN=letra;
+                }
+        }
+        decision=confirm("Desea continuar")
     }
+    promedio=positivo/contador;
+    console.log("cantidad de pares= "+par);
+    console.log("cantidad de impares= "+impar);
+    console.log("cantidad de ceros"+ ceros);
+    console.log("promedio de numeros positivos = "+promedio);
+    console.log("suma de negativos"+ negativos);
+    console.log("numero y letra del maximo "+maximo+" "+letraMAX+" y minimo "+minimo+" "+letraMIN)
+      
+    
 }
